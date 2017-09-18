006e0878592ef34003d78e8017d4202f5fbe5ce0
Alexandro <arvitaly@list.ru>
2017-09-18 05:52:34 +0300

    fix types

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { JSDOM } from "jsdom";
 import fetch from "node-fetch";
 export { sel, attr, obj, css, child, html, hasClass, IModel, GrabberO, nextUntil, text, val } from "page-grabber";
 import Grabber = require("page-grabber");
-const grab = async <T> (url: string, model: T) => {
+const grab = async <T> (url: string, model: T): Promise<T> => {
     const res = await fetch(url);
     if (res.status !== 200) {
         throw new Error("Invalid request, response status " + res.status + ":" + res.statusText + "\n" + res.text());
